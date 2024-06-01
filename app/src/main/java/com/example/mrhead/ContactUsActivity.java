fc4c19ef7867efa0c4b2000459d3a1b9419ec831
akmalfman <akmalfsalman2@gmail.com>
2024-06-01 17:12:40 +0700

    add component in ContactUsActivity

--- app/src/main/java/com/example/mrhead/ContactUsActivity.java
+++ app/src/main/java/com/example/mrhead/ContactUsActivity.java
@@ -1,24 +1,55 @@
 package com.example.mrhead;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class ContactUsActivity extends AppCompatActivity {
+
+    private Button btnCall, btnEmail, btnWebsite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_contact_us);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        btnCall = findViewById(R.id.button_call);
+        btnEmail = findViewById(R.id.button_email);
+        btnWebsite = findViewById(R.id.button_web);
+
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:+6285156760830"));
+                startActivity(intent);
+            }
+        });
+
+        btnEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SENDTO);
+                intent.setData(Uri.parse("mailto:akmalfsalman2@gmail.com"));
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Judul Email");
+                intent.putExtra(Intent.EXTRA_TEXT, "Isi Email disini");
+                startActivity(intent);
+            }
+        });
+
+        btnWebsite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = "+6285156760830"; // Ganti dengan nomor telepon yang Anda inginkan
+                String url = "https://api.whatsapp.com/send?phone=" + phoneNumber;
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
         });
     }
 }
